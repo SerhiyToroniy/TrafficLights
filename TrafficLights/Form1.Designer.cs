768041da6baca2fe668cdba3e1e2468a4b4e7cf9
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2022-05-13 18:05:57 +0300

    CircleGame

--- TrafficLights/Form1.Designer.cs
+++ TrafficLights/Form1.Designer.cs
@@ -29,9 +29,7 @@ namespace TrafficLights
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -39,49 +37,28 @@ namespace TrafficLights
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button4 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(342, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Manual";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(342, 54);
+            this.button2.Location = new System.Drawing.Point(948, 63);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Auto";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.numericUpDown3);
-            this.groupBox1.Controls.Add(this.numericUpDown2);
-            this.groupBox1.Controls.Add(this.numericUpDown1);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Enabled = false;
-            this.groupBox1.Location = new System.Drawing.Point(262, 103);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(155, 284);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Duration(in ms)";
             // 
             // label1
             // 
@@ -186,9 +163,26 @@ namespace TrafficLights
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(868, 112);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 284);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Duration(in ms)";
+            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(300, 405);
+            this.button4.Location = new System.Drawing.Point(906, 414);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 7;
@@ -196,41 +190,98 @@ namespace TrafficLights
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Час(t): ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Результат(N): ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(357, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Спроби(P=): ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Рівень: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(479, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Найкращий результат:  ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.ClientSize = new System.Drawing.Size(719, 619);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Mergen Meredow";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Гра \"Кулька\"";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Shown += new System.EventHandler(this.Form1_Shown);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.NumericUpDown numericUpDown3;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- TrafficLights/Form1.cs
+++ TrafficLights/Form1.cs
@@ -1,49 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TrafficLights
 {
     public partial class Form1 : Form
     {
-        Color def;
+        int t = 3000;
+        int N = 1;
+        int P = 5;
+
+
         Color forecolor1;
-        Color forecolor2;
-        Color forecolor3;
         int lineWidth = 2;
-        int centerX = 100;
-        int centerY = 120;
-        int radius = 50;
         Graphics g;
         SolidBrush myBrush;
         Pen myPen;
-        int num = 1;
-        int sleep1;
-        int sleep2;
-        int sleep3;
-        Thread drawingAuto;
-        bool goUp = false;
+
+        int t_decrement = 100;
+        int level = 1;
+        int score = 0;
+        int trys = 0;
+        int succ_trys = 0;
+
+        int minX;
+        int minY;
+
+        int maxX;
+        int maxY;
+        int x = 0;
+        int y = 0;
+        int r = 20;
+        bool drawn = false;
+
+        int bestScore = 0;
+        Random random;
+        Thread thread;
+        int mouseClicked = 0;
+
         public Form1()
         {
             InitializeComponent();
+            minX = 50 + r;
+            minY = 50 + r;
+            maxX = Width - minX - r;
+            maxY = Height - minY - r;
             forecolor1 = Color.Red;
-            forecolor2 = Color.Yellow;
-            forecolor3 = Color.Green;
-            def = Color.Gray;
-            sleep1 = Convert.ToInt32(numericUpDown1.Value);
-            sleep2 = Convert.ToInt32(numericUpDown2.Value);
-            sleep3 = Convert.ToInt32(numericUpDown3.Value);
             g = CreateGraphics();
-            myPen = new Pen(forecolor1, lineWidth);
-            myBrush = new SolidBrush(forecolor1);
-
+            random = new Random();
+            label7.Text += level;
+            label4.Text += t;
+            label5.Text += score;
+            label6.Text = $"Спроби(P={P}): {trys}";
+            string fileContent = File.ReadAllText("../../Best Scores.txt");
+            if (int.TryParse(fileContent, out int bestScore))
+            {
+                this.bestScore = bestScore;
+            }
+            else this.bestScore = 0;
+            label8.Text += this.bestScore;
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -53,46 +70,7 @@ namespace TrafficLights
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (drawingAuto != null)
-                drawingAuto.Abort();
-            groupBox1.Enabled = false;
-            myPen = new Pen(def, lineWidth);
-            myBrush = new SolidBrush(def);
-            g.FillCircle(myBrush, centerX, centerY, radius);
-            g.DrawCircle(myPen, centerX, centerY, radius);
 
-            g.FillCircle(myBrush, centerX, centerY + radius * 2 + 20, radius);
-            g.DrawCircle(myPen, centerX, centerY + radius * 2 + 20, radius);
-
-            g.FillCircle(myBrush, centerX, centerY + radius * 4 + 40, radius);
-            g.DrawCircle(myPen, centerX, centerY + radius * 4 + 40, radius);
-            switch (num)
-            {
-                case 1:
-                    goUp = false;
-                    myPen = new Pen(forecolor1, lineWidth);
-                    myBrush = new SolidBrush(forecolor1);
-                    g.FillCircle(myBrush, centerX, centerY, radius);
-                    g.DrawCircle(myPen, centerX, centerY, radius);
-                    break;
-                case 2:
-                    myPen = new Pen(forecolor2, lineWidth);
-                    myBrush = new SolidBrush(forecolor2);
-                    g.FillCircle(myBrush, centerX, centerY + radius * 2 + 20, radius);
-                    g.DrawCircle(myPen, centerX, centerY + radius * 2 + 20, radius);
-                    break;
-                case 3:
-                    goUp = true;
-                    myPen = new Pen(forecolor3, lineWidth);
-                    myBrush = new SolidBrush(forecolor3);
-                    g.FillCircle(myBrush, centerX, centerY + radius * 4 + 40, radius);
-                    g.DrawCircle(myPen, centerX, centerY + radius * 4 + 40, radius);
-                    break;
-            }
-            if(!goUp)
-                num++;
-            else
-                num--;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -101,87 +79,94 @@ namespace TrafficLights
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-            g.FillCircle(myBrush, centerX, centerY, radius);
-            g.DrawCircle(myPen, centerX, centerY, radius);
-            myPen = new Pen(def, lineWidth);
-            myBrush = new SolidBrush(def);
-            g.FillCircle(myBrush, centerX, centerY + radius * 2 + 20, radius);
-            g.DrawCircle(myPen, centerX, centerY + radius * 2 + 20, radius);
-
-            g.FillCircle(myBrush, centerX, centerY + radius * 4 + 40, radius);
-            g.DrawCircle(myPen, centerX, centerY + radius * 4 + 40, radius);
+            thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(t);
+                    g.Clear(BackColor);
+                    drawn = false;
+                    x = random.Next(minX, maxX);
+                    y = random.Next(minY, maxY);
+                    myPen = new Pen(forecolor1, lineWidth);
+                    myBrush = new SolidBrush(forecolor1);
+                    g.FillCircle(myBrush, x, y, r);
+                    g.DrawCircle(myPen, x, y, r);
+                    mouseClicked = 0;
+                    drawn = true;
+                }
+            });
+            thread.Start();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (drawingAuto != null)
-                drawingAuto.Abort();
-            myPen = new Pen(forecolor1, lineWidth);
-            myBrush = new SolidBrush(forecolor1);
-            g.FillCircle(myBrush, centerX, centerY, radius);
-            g.DrawCircle(myPen, centerX, centerY, radius);
-            myPen = new Pen(def, lineWidth);
-            myBrush = new SolidBrush(def);
-            g.FillCircle(myBrush, centerX, centerY + radius * 2 + 20, radius);
-            g.DrawCircle(myPen, centerX, centerY + radius * 2 + 20, radius);
 
-            g.FillCircle(myBrush, centerX, centerY + radius * 4 + 40, radius);
-            g.DrawCircle(myPen, centerX, centerY + radius * 4 + 40, radius);
-            sleep1 = Convert.ToInt32(numericUpDown1.Value);
-            sleep2 = Convert.ToInt32(numericUpDown2.Value);
-            sleep3 = Convert.ToInt32(numericUpDown3.Value);
-            drawingAuto = new Thread(() =>
-            {
-                while (true)
-                {
-                    myPen = new Pen(def, lineWidth);
-                    myBrush = new SolidBrush(def);
-                    g.FillCircle(myBrush, centerX, centerY, radius);
-                    g.DrawCircle(myPen, centerX, centerY, radius);
-
-                    g.FillCircle(myBrush, centerX, centerY + radius * 2 + 20, radius);
-                    g.DrawCircle(myPen, centerX, centerY + radius * 2 + 20, radius);
-
-                    g.FillCircle(myBrush, centerX, centerY + radius * 4 + 40, radius);
-                    g.DrawCircle(myPen, centerX, centerY + radius * 4 + 40, radius);
-                    switch (num)
-                    {
-                        case 1:
-                            goUp = false;
-                            myPen = new Pen(forecolor1, lineWidth);
-                            myBrush = new SolidBrush(forecolor1);
-                            g.FillCircle(myBrush, centerX, centerY, radius);
-                            g.DrawCircle(myPen, centerX, centerY, radius);
-                            Thread.Sleep(sleep1);
-                            break;
-                        case 2:
-                            myPen = new Pen(forecolor2, lineWidth);
-                            myBrush = new SolidBrush(forecolor2);
-                            g.FillCircle(myBrush, centerX, centerY + radius * 2 + 20, radius);
-                            g.DrawCircle(myPen, centerX, centerY + radius * 2 + 20, radius);
-                            Thread.Sleep(sleep2);
-                            break;
-                        case 3:
-                            goUp = true;
-                            myPen = new Pen(forecolor3, lineWidth);
-                            myBrush = new SolidBrush(forecolor3);
-                            g.FillCircle(myBrush, centerX, centerY + radius * 4 + 40, radius);
-                            g.DrawCircle(myPen, centerX, centerY + radius * 4 + 40, radius);
-                            Thread.Sleep(sleep3);
-                            break;
-                    }
-                    if (!goUp)
-                        num++;
-                    else
-                        num--;
-                }
-            });
-            drawingAuto.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            groupBox1.Enabled = true;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (mouseClicked < 1)
+            {
+                mouseClicked += 1;
+                trys += 1;
+                if (drawn)
+                {
+                    var relativePoint = PointToClient(Cursor.Position);
+                    var currentX = x;
+                    var currentY = y;
+                    if (Math.Pow(relativePoint.X - currentX, 2) + Math.Pow(relativePoint.Y - currentY, 2) < Math.Pow(r, 2))
+                    {
+                        succ_trys += 1;
+                        score += N;
+                    }
+                }
+                //game over
+                if (trys >= P)
+                {
+                    if (succ_trys != 3)
+                    {
+                        //write to file
+                        if (score >= bestScore)
+                            File.WriteAllText("../../Best Scores.txt", score.ToString());
+
+                        label6.Text = $"Спроби(P={P}): {trys}";
+                        if (thread != null)
+                            thread.Abort();
+                        MessageBox.Show($"Ваш результат - {score}","Гра завершена!");
+                        Close();
+                    }
+                }
+                //new level
+                else
+                {
+                    if (succ_trys == 3)
+                    {
+                        level += 1;
+                        t -= t_decrement;
+                        succ_trys = 0;
+                        trys = 0;
+
+                        //write to file
+                        if (score >= bestScore)
+                            File.WriteAllText("../../Best Scores.txt", score.ToString());
+                    }
+                }
+                label7.Text = $"Рівень: {level}";
+                label4.Text = $"Час(t): {t}";
+                label5.Text = $"Результат(N): {score}";
+                label6.Text = $"Спроби(P={P}): {trys}";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (thread != null)
+                thread.Abort();
         }
     }
 }
